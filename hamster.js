5b8626897e8f9dacb34d4e931f0403bc67bde6de
xi chen <chenxigirl@gmail.com>
2016-06-14 21:27:26 -0500

    hamster

--- hamster.js
+++ hamster.js
@@ -2,24 +2,28 @@ var request = require('request');
 var open = require('open');
 
 // 1. Find the correct URL to get adorable hamsters from Giphy!
-request('GIPHY API GET REQUEST URL', function (error, response, body) {
+request('http://api.giphy.com/v1/gifs/search?q=birthday+cake&api_key=dc6zaTOxFJmzC', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var response_hash = JSON.parse(body);
     console.log(response_hash);
 
-    // 2. Set up an array variable `data_array` that stores the array of GIF hashes. HINT: You'll want to grab the value of the "data" key in the `response_hash` object.
-    // var data_array = ???;
+    // 2. Set up an array variable `data_array` that stores the array of GIF hashes.
+    //HINT: You'll want to grab the value of the "data" key in the `response_hash` object.
+    var data_array =response_hash.data ;
 
-    // 3. Use the `map()` function to iterate through the `data_array` array. For each `gif_hash` in `data_array`, return the GIF URL.
-    // var url_array = data_array.map(function(gif_hash){
-    //   ???
-    // })
+    // 3. Use the `map()` function to iterate through the `data_array` array. For each `gif_hash` in `data_array`,
+    //return the GIF URL.
+    var url_array = data_array.map(function(gif_hash){
+      var GIFURL=gif_hash.url;
+      return (GIFURL);
+      }
+    });
 
     // 4. Log the `url_array` to your console.
-    // console.log(url_array);
+    console.log(url_array);
 
     // 5. Uncomment this once it works to open a random GIF in your browser!
-    // openRandom(url_array);
+    openRandom(url_array);
   }
 })
 
